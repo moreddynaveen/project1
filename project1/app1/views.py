2daa8d1c1b726544e3fb1f286d9ee7f319632fe8
moreddynaveen <moreddynaveenreddy@gmail.com>
2023-04-23 16:06:56 +0530

    firstcommit

---
+++ project1/app1/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+#create your viws here.
+def fun1(request):
+	return HttpResponse('<h1>thanks to BSNV SATYANARAYANA</h1>')
+
+
